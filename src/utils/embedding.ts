fbaca7f0353a8ee904b710e6a55ee036db58ea84
Mehul Prajapati <ailearn90@gmail.com>
2026-01-16 20:45:58 +0530

    feat: Init Pinecone for vector DB, added vectorization function

---
+++ src/utils/embedding.ts
@@ -0,0 +1,15 @@
+import { GoogleGenAI } from "@google/genai";
+
+export const createEmbedding = async (content: string) => {
+    const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_GEMINI_API_KEY! });
+
+    const response = await ai.models.embedContent({
+        model: 'text-embedding-004',
+        contents: content.trim().split("\n").map((line) => line.trim()),
+        config: { 
+            outputDimensionality: 768,
+        },
+    });
+
+    return response?.embeddings?.[0]?.values ?? [];
+};

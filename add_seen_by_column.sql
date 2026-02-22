f564015e7749076c8557e8433a5a818c19b6cb27
Mehul Prajapati <ailearn90@gmail.com>
2026-02-22 18:33:47 +0530

    fix: google auth redirect fix

--- add_seen_by_column.sql
+++
@@ -1,5 +0,0 @@
--- Add seenBy column to Message table
-ALTER TABLE "Message" ADD COLUMN IF NOT EXISTS "seenBy" TEXT[] DEFAULT '{}';
-
--- Add comment
-COMMENT ON COLUMN "Message"."seenBy" IS 'Array of user IDs who have seen this message';
